8ceccea5d651346269d654b603b934663739f11d
Matthew Elgert <mdelgert@yahoo.com>
2025-05-25 00:04:59 -0400

    Add build script and initial .gitignore and tag files

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-debuild -us -uc
+# This script builds the package using debuild
+# and creates a .deb file in the parent directory.
+
+# Delete previous build artifacts
+rm -rf ../*.deb ../*.changes ../*.build ../*.buildinfo
+rm -rf ../*.dsc ../*.tar.xz ../*.tar.gz
+
+#debuild -us -uc
